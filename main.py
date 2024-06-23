2a71c9720652b7946330d9ea688b23b8cf336b1a
Aidan Christie <aidanchristie2232@gmail.com>
2024-06-23 06:56:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
-# This is a sample Python script.
+import time
+#
+projectVersion = 1.3
+adminLogin = False
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def adminLogin_Check():
+    adminLogin_verify = input(f"Please enter the admin login code: ")
 
+    pass
+def generatePassword():
+    print(f"Generate password func")
+    time.sleep(2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    passwordType_select = input(f"")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Main true loop
+while True:
+    time.sleep(2.5)
+    # Main menu loop
+    adminLogin_Check()
+    mainMenu_Input = input(f"Choose an option: "
+                           f"\n\t1. Generate a password"
+                           f"\n\t2. View passwords"
+                           f"\n\t3. View accounts"
+                           f"\n\t4. Add account"
+                           f"\n\t5. Delete account"
+                           f"\n\t6. Show help "
+                           f"\n\t7. Options"
+                           f"\n\t8. Exit"
+                           f"\n\nEnter input: ")
+    # Check if input is valid
+    try:
+        val = int(mainMenu_Input)
+    except ValueError:
+        print(f"Not a digit")
+    # Check if input is valid
+    if val not in (1, 2, 3, 4, 5, 6, 7, 8):
+        print(f"Please select a valid option!")
+        continue
+    match val:
+        # Generate password
+        case 1:
+            generatePassword()
+        # View passwords
+        case 2:
+            print("> View password")
+            continue
+        # View accounts
+        case 3:
+            print("> View accounts")
+            continue
+        # Add account
+        case 4:
+            print("> Add account")
+            continue
+        # Delete account
+        case 5:
+            print("> Delete account")
+            continue
+        # Help menu
+        case 6:
+            print("> Open help menu")
+            continue
+        # Application options
+        case 7:
+            print("> Options")
+            continue
+        # Exit program
+        case 8:
+            quit()
